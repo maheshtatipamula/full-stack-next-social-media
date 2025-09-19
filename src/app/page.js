9cbd27478b7c2f9803502fba2e154cb3e8e60b51
mahesh goud tatipamula <108403824+maheshtatipamula@users.noreply.github.com>
2025-09-19 10:08:30 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,29 +6,9 @@ import { useRouter } from "next/navigation";
 const Page = () => {
   const router = useRouter();
 
-async function getLocationFromIP() {
-    try {
-        const res = await fetch("http://ip-api.com/json/");
-        const data = await res.json();
-        console.log("22 data", data);
-
-        return {
-            ip: data.query,
-            city: data.city,
-            region: data.regionName,
-            country: data.country,
-            lat: data.lat,
-            lon: data.lon,
-        };
-    } catch (err) {
-        console.error("Error fetching IP location:", err);
-        return null;
-    }
-}
 
   useEffect(() => {
-     console.log("getLocationFromIP")
-     getLocationFromIP() 
+ 
     router.replace("/dashboard");
   }, []);
   return null;
