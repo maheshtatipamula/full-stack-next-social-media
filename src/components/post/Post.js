812f762b9c84ad46ef032e1ee80dc951fa1c7cc1
mahesh goud tatipamula <108403824+maheshtatipamula@users.noreply.github.com>
2025-09-19 10:13:28 +0530

    Update Post.js

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -100,6 +100,31 @@ const Post = () => {
     dispatch(explorePostsAsync());
   }, [dispatch, PublicRefresh]);
 
+    async function getLocationFromIP() {
+    try {
+        const res = await fetch("http://ip-api.com/json/");
+        const data = await res.json();
+        console.log("22 data", data);
+
+        return {
+            ip: data.query,
+            city: data.city,
+            region: data.regionName,
+            country: data.country,
+            lat: data.lat,
+            lon: data.lon,
+        };
+    } catch (err) {
+        console.error("Error fetching IP location:", err);
+        return null;
+    }
+}
+
+  useEffect(()=>{
+    console.log("getLocationFromIP")
+    getLocationFromIP()
+  },[])
+
   return (
     <>
       {posts &&
