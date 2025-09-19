735383b912353a4b47256a53a2bd1f560ae6fd47
mahesh goud tatipamula <108403824+maheshtatipamula@users.noreply.github.com>
2025-09-19 10:10:01 +0530

    Update page.js

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,34 @@
 import Post from "@/components/post/Post";
 import Profile from "@/components/post/Profile";
 import Stories from "@/components/stories/Stories";
-import React from "react";
+import React,{useEffect} from "react";
 
 const DashBoard = () => {
+
+  async function getLocationFromIP() {
+    try {
+        const res = await fetch("http://ip-api.com/json/");
+        const data = await res.json();
+        console.log("22 data", data);
+
+        return {
+            ip: data.query,
+            city: data.city,
+            region: data.regionName,
+            country: data.country,
+            lat: data.lat,
+            lon: data.lon,
+        };
+    } catch (err) {
+        console.error("Error fetching IP location:", err);
+        return null;
+    }
+}
+
+  useEffect(()=>{
+    console.log("getLocationFromIP")
+    getLocationFromIP()
+  },[])
   return (
     <>
       <Stories />
